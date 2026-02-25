62845cc3e93d348841d85def8a6d792230f50ed8
Archit Shah <shaharchit07@gmail.com>
2026-02-25 16:50:12 -0500

    check in product page components

--- frontend/src/landing_page/products/Hero.jsx
+++ frontend/src/landing_page/products/Hero.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React from "react";
 
 function Hero() {
-    return ( <div>Products Hero Section</div> );
+  return (
+    <div className="container pt-5 border-bottom mb-5">
+      <div className="row text-center">
+        <h1 className="fs-2 mb-4">Technology</h1>
+        <h3 className="text-muted mb-4" style={{fontSize:"1rem"}}>Sleek, modern and intuitive trading platforms</h3>
+        <p style={{fontSize:"0.8rem"}}>Check out our <a href="" style={{textDecoration:"None"}}>investment offerings <i className="fa fa-long-arrow-right"></i></a></p>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
--- frontend/src/landing_page/products/RightSection.jsx
+++ frontend/src/landing_page/products/RightSection.jsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import React from "react";
 
-function RightSection() {
-    return ( <div>Products Right Section</div> );
+function RightSection({  
+  imageUrl,
+  productName,
+  productDescription,
+  learnMore
+ }) {
+  return (
+    <div className="container mt-5">
+      <div className="row">
+
+
+        <div className="col-6 p-5 mt-5">
+          <h1>{productName}</h1>
+          <p style={{ lineHeight: "1.8" }}>{productDescription}</p>
+
+          <div>
+            <a
+              href={learnMore}
+              style={{ textDecoration: "none" }}
+            >
+              Learn More <i className="fa fa-long-arrow-right"></i>
+            </a>
+          </div>
+
+         
+        </div>
+
+        <div className="col-6">
+          <img src={imageUrl} alt="" />
+        </div>
+
+        
+      </div>
+    </div>
+  );
 }
 
 export default RightSection;
